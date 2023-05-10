518ffabfcbeaa2e6161a925221fc6a55131048fa
xsheng <2838428014@qq.com>
2023-05-10 19:33:14 +0800

    init

---
+++ src/main/java/com/smarthousekeeping/SmartHouseKeepingApplication.java
@@ -0,0 +1,13 @@
+package com.smarthousekeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHouseKeepingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHouseKeepingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smarthousekeeping/SmartHouseKeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthousekeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHouseKeepingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
